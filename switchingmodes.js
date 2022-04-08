ffa7a07f07016bcf1da66280a9fa063ca10a7c82
Sprinklyx <ryeshat@gmail.com>
2022-04-07 20:21:27 -0400

    figuring out file usage

--- switchingmodes.js
+++ switchingmodes.js
@@ -1,8 +1,14 @@
+document.addEventListener("index.html", switchingmodes()); 
+
 var darkButton = document.createElement("button");
 darkButton.innerHTML = "Dark Mode";
+darkButton.className = "cdma, cdmd, cdmh";
 
 darkButton.onclick = switchingmodes();
 function switchingmodes() {
     var pageState = document.body;
-    pageState.classList.toggle("custom-dark-mode");
-}
+    pageState.classList.toggle(darkButton.className);
+};
+
+var toAddDark = document.getElementById("dark mode");
+toAddDark.appendChild(darkButton);
